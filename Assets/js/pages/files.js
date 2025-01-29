2c1be937e780136948844604e75b9c977d13c748
JairoMJuarezM <jairojuarez200@gmail.com>
2025-01-29 08:56:05 -0600

    Initial commit

---
+++ Assets/js/pages/files.js
@@ -0,0 +1,6 @@
+const btnUpload = document.querySelector('#btnupload');
+document.addEventListener('DOMContentLoaded', function(){
+    btnUploadUpload.addEventListener('click', function(){
+        
+    })
+})
